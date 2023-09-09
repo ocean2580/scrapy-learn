d12223f9701ade6003d539315e356d1412eed551
ocean2580 <2080917477@qq.com>
2023-09-09 15:06:05 +0800

    modify: process and clean data in hash with pipelines.py

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -1,37 +1,34 @@
 import scrapy
-from bookscraper.bookscraper.items import BookItem
+from bookscraper.items import BookItem
+
 
 class BookspiderSpider(scrapy.Spider):
-    name = "bookspider"
-    allowed_domains = ["books.toscrape.com"]
-    start_urls = ["https://books.toscrape.com"]
-
-
+    name = 'bookspider'
+    allowed_domains = ['books.toscrape.com']
+    start_urls = ['https://books.toscrape.com/']
 
     def parse(self, response):
         books = response.css('article.product_pod')
-        # each book information
         for book in books:
-            relative_url = book.css("h3 a::attr('href')").get()
+            relative_url = book.css('h3 a ::attr(href)').get()
 
             if 'catalogue/' in relative_url:
-                book_url = "https://books.toscrape.com/" + relative_url
+                book_url = 'https://books.toscrape.com/' + relative_url
             else:
-                book_url = "https://books.toscrape.com/catalogue/" + relative_url
+                book_url = 'https://books.toscrape.com/catalogue/' + relative_url
             yield response.follow(book_url, callback=self.parse_book_page)
-        # next page
-        next_page = response.css('li.next a::attr(href)').get()
 
+        next_page = response.css('li.next a ::attr(href)').get()
         if next_page is not None:
             if 'catalogue/' in next_page:
-                next_page_url = "https://books.toscrape.com/" + next_page
+                next_page_url = 'https://books.toscrape.com/' + next_page
             else:
-                next_page_url = "https://books.toscrape.com/catalogue/" + next_page
-
+                next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
             yield response.follow(next_page_url, callback=self.parse)
 
     def parse_book_page(self, response):
-        table_rows = response.css('table tr')
+
+        table_rows = response.css("table tr")
         book_item = BookItem()
 
         book_item['url'] = response.url,
@@ -49,4 +46,4 @@ class BookspiderSpider(scrapy.Spider):
         book_item['description'] = response.xpath("//div[@id='product_description']/following-sibling::p/text()").get(),
         book_item['price'] = response.css('p.price_color ::text').get(),
 
-        yield BookItem
+        yield book_item
